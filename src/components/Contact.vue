6314bbc701e8b356c7193d41627b83858ef3252c
Jack Williams <jack.w141@outlook.com>
2024-06-25 10:23:24 +0100

    form for netlify

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,9 +4,10 @@
         <p class="contact__description">Get in touch with me via email and I'll reply as fast as I can to your enquiry!
         </p>
         <!-- <form action="https://api.web3forms.com/submit" method="POST" aria-label="Contact Form" netlify> -->
-        <form name="contact" action="/thanks" method="POST" aria-label="Contact Form" netlify>
+        <form name="contact" action="/thanks" method="POST" aria-label="Contact Form" netlify data-netlify="true"
+            data-netlify-honeypot="bot-field">
             <!-- <input type="hidden" name="access_key" value="7364cf4f-a2d8-4e74-87fd-8f078b9b10a4"> -->
-            <input type="hidden" name="contact" value="contact" netlify>
+            <input type="hidden" name="form-name" value="ask-question" />
             <label for="name">Name</label>
             <input class="contact__name" type="text" name="name" id="name" required aria-required="true">
             <label for="email">Email</label>
